05ce3cf4135a414beff2df33d455d5a0b976a81d
carlosreyes210177 <carlosreyes210177@gmail.com>
2019-01-08 16:26:13 -0600

    Add files via upload

---
+++ Cap_2/Dll.ClasOperation.pas
@@ -0,0 +1,96 @@
+﻿unit Dll.ClasOperation;
+
+interface
+uses
+   System.SysUtils,
+   System.Classes;
+
+type
+   IOperation = interface
+   ['{655F5196-7EBB-473F-BE76-99B33883A59D}']
+      function Suma(const iA : Integer; const iB : Integer) : Integer;
+      function Resta(const iA : Integer; const iB : Integer) : Integer;
+      function Dividir(const iA : Integer; const iB : Integer) : Integer;
+      function Multiplicar(const iA : Integer; const iB : Integer) : Integer;
+   end;
+
+   TOperation = class(TInterfacedObject, IOperation)
+   private
+
+
+   public
+      constructor Create;
+      destructor Destroy; override;
+
+      function Suma(const iA : Integer; const iB : Integer) : Integer;
+      function Resta(const iA : Integer; const iB : Integer) : Integer;
+      function Dividir(const iA : Integer; const iB : Integer) : Integer;
+      function Multiplicar(const iA : Integer; const iB : Integer) : Integer;
+   end;
+
+
+implementation
+
+{ TOperation }
+
+constructor TOperation.Create;
+begin
+
+end;
+
+destructor TOperation.Destroy;
+begin
+
+   inherited Destroy;
+end;
+
+function TOperation.Dividir(const iA, iB: Integer): Integer;
+begin
+   try
+      Result := Trunc(ia / iB);
+   except
+      on E : Exception do
+      begin
+         raise Exception.Create(e.Message);
+      end;
+   end;
+end;
+
+function TOperation.Multiplicar(const iA, iB: Integer): Integer;
+begin
+   try
+      Result := Trunc(ia * iB);
+   except
+      on E : Exception do
+      begin
+         raise Exception.Create(e.Message);
+      end;
+   end;
+end;
+
+function TOperation.Resta(const iA, iB: Integer): Integer;
+begin
+   try
+      Result := (ia - iB);
+   except
+      on E : Exception do
+      begin
+         raise Exception.Create(e.Message);
+      end;
+   end;
+
+end;
+
+function TOperation.Suma(const iA, iB: Integer): Integer;
+begin
+   try
+      Result := (ia + iB);
+   except
+      on E : Exception do
+      begin
+         raise Exception.Create(e.Message);
+      end;
+   end;
+end;
+
+end.
---
+++ Cap_2/Operations.dpr
@@ -0,0 +1,54 @@
+﻿library Operations;
+
+{ Important note about DLL memory management: ShareMem must be the
+  first unit in your library's USES clause AND your project's (select
+  Project-View Source) USES clause if your DLL exports any procedures or
+  functions that pass strings as parameters or function results. This
+  applies to all strings passed to and from your DLL--even those that
+  are nested in records and classes. ShareMem is the interface unit to
+  the BORLNDMM.DLL shared memory manager, which must be deployed along
+  with your DLL. To avoid using BORLNDMM.DLL, pass string information
+  using PChar or ShortString parameters. }
+
+uses
+  System.SysUtils,
+  System.Classes,
+  Dll.ClasOperation in 'Dll.ClasOperation.pas';
+
+{$R *.res}
+
+function Suma(iA : Integer; iB : Integer) : Integer; stdcall;
+begin
+   try
+      Result  := (iA + iB);
+   except
+      on E : Exception do
+      begin
+         raise Exception.Create(E.Message);
+      end;
+   end;
+end;
+
+function Resta(iA : Integer; iB : Integer) : Integer; stdcall;
+begin
+   Result := (iA - iB);
+end;
+
+function Multiplicar(iA : Integer; iB : Integer) : Integer; stdcall;
+begin
+   Result := Trunc(iA * iB);
+end;
+
+function Dividir(iA : Integer; iB : Integer) : Integer; stdcall;
+begin
+   Result := Trunc(iA / iB);
+end;
+
+exports Suma;
+exports Resta;
+exports Multiplicar;
+exports Dividir;
+
+begin
+
+end.
